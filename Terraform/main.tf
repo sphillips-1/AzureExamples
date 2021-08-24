9946fc28379f54c9116c61a4b605619c98aef3fe
Sean Phillips <sean-phillips@live.com>
2021-08-24 12:27:07 -0400

    modified azurerm requirements

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,14 +1,11 @@
 terraform {
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
+    azurerm = "~> 2.65"
   }
 
   required_version = ">= 0.14.9"
-  
+
 }
 
 
