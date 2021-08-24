77da2708cebc87d0c4ca0fdee5f6a5ff9870cdb7
Sean Phillips <sean-phillips@live.com>
2021-08-24 12:23:50 -0400

    Modified terraform {}

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,14 +1,17 @@
 terraform {
 
-  required_version = ">=1.0.0"
-  
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
     }
   }
+
+  required_version = ">= 0.14.9"
+  
 }
+
+
 
 provider "azurerm" {
   features {}
